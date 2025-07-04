011647d2032ee8f87367c55a34161559800d9c5e
igrapel <grapel.ilan@gmail.com>
2025-07-04 00:01:13 -0400

    added dynamic page

---
+++ src/data/booklets.js
@@ -0,0 +1,17 @@
+// src/data/booklets.js
+export const booklets = [
+  {
+    id: "1",
+    title: "Jewish Months Poster",
+    preview: "/booklets/heb_poster.jpeg",
+    image: "/images/heb_poster.jpeg",
+    stripeUrl: "https://buy.stripe.com/test_abc123",
+  },
+  {
+    id: "2",
+    title: "Hebrew - Level 2",
+    preview: "/booklets/humash.pdf",
+    image: "/images/humash.jpg",
+    stripeUrl: "https://buy.stripe.com/test_abc123",
+  },
+];
